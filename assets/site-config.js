5962623be6ccb5abb27eddc7599b8c7302d8b723
Bcro22 <bretcrofoot@gmail.com>
2026-08-11 14:40:50 -0400

    payment link test

--- assets/site-config.js
+++ assets/site-config.js
@@ -2,7 +2,7 @@
 // Leave blank until connected; the site falls back to the service/intake page.
 window.CAC_CONFIG = {
   scheduleBuilderCall: "",
-  stripeFocusedReview: "",
-  stripeComprehensiveReview: "",
+  stripeFocusedReview: "https://buy.stripe.com/test_3cI3cxbhI5xYdkTdZ4enS01",
+  stripeComprehensiveReview: "https://buy.stripe.com/test_bJe5kFbhIaSi80z9IOenS00",
   customRequest: "contact.html"
 };
